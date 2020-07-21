caa82463c5d1e9df3f93089eb3f78a42f3eccd56
Lucca <lucca.marques08@gmail.com>
2020-07-21 19:23:03 -0300

    Login page and User page created; collapse added in User page

--- vrtx-kl_web/vrtxkl.js
+++ vrtx-kl_web/vrtxkl.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
     $("#searchBtn").on("click", function() {
     var value = $("#inputData").val().toLowerCase();
-    $("#tableData tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
+        $("#tbodyData tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
 });
